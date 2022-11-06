33cc6d2f03de8d753e15e52a5b88ebb30a70cb44
m1thraz <boehser-.-onkel@hotmail.de>
2022-11-06 18:50:38 +0200

    added GameOver Scene

--- Assets/PlayerLogic.cs
+++ Assets/PlayerLogic.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerLogic : MonoBehaviour
 {
@@ -32,6 +33,7 @@ public class PlayerLogic : MonoBehaviour
         {
             Debug.Log("Player DIED");
             Destroy(gameObject);
+            SceneManager.LoadScene(2);
         }
     }
 
