676780d115907eba75cc38e7677fee5e5bd60d92
m1thraz <boehser-.-onkel@hotmail.de>
2022-11-06 17:31:38 +0200

    health hud is now working

--- Assets/PlayerLogic.cs
+++ Assets/PlayerLogic.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerLogic : MonoBehaviour
 {
+    public TextMeshProUGUI HPText;
+    public TextMeshProUGUI ScoreText;
     // Start is called before the first frame update
-    [SerializeField] float maxHealth = 3, health;
+    [SerializeField] float maxHealth = 3, health, score;
     void Start()
     {
         health = maxHealth;
         Physics.IgnoreLayerCollision(0, 6);
+        HPText.text = "Health: " + health.ToString();
     }
 
     // Update is called once per frame
@@ -21,6 +25,7 @@ public class PlayerLogic : MonoBehaviour
     public void takeDamage(float damageAmount)
     {
         health -= damageAmount;
+        HPText.text = "Health : " +health.ToString();
         Debug.Log(string.Format("Player has now {0} health left", health));
         if (health <= 0)
         {
