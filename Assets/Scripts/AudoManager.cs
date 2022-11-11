6ca7798a051944e95d75be067890c0ac77f23cff
m1thraz <boehser-.-onkel@hotmail.de>
2022-11-11 17:15:16 +0200

    edited audiomanager

--- Assets/Scripts/AudoManager.cs
+++ Assets/Scripts/AudoManager.cs
@@ -7,9 +7,16 @@ public class AudoManager : MonoBehaviour
     
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach( Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
     }
 
     // Update is called once per frame
