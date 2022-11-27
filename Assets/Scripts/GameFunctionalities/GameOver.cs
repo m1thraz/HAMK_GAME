89c35c164921de50835e88d61fc1481c113ec2e0
m1thraz <boehser-.-onkel@hotmail.de>
2022-11-27 02:00:19 +0200

    added persistend gameover score

--- Assets/Scripts/GameFunctionalities/GameOver.cs
+++ Assets/Scripts/GameFunctionalities/GameOver.cs
@@ -7,10 +7,27 @@ using TMPro;
 public class GameOver : MonoBehaviour
 {
     public TextMeshProUGUI Score;
+    public TextMeshProUGUI HighScore;
 
     public void Start()
     {
-        Score.text = "Score: " + PlayerPrefs.GetFloat("playScore"); ;
+        Score.text = "Score: " + PlayerPrefs.GetFloat("playScore"); 
+        if (!PlayerPrefs.HasKey("highScore"))
+        {
+            HighScore.text = "Highscore: " + PlayerPrefs.GetFloat("playScore");
+            PlayerPrefs.SetFloat("highScore", PlayerPrefs.GetFloat("playScore"));
+        } else
+        {
+            if(PlayerPrefs.GetFloat("playScore") < PlayerPrefs.GetFloat("highScore"))
+            {
+                HighScore.text ="Highscore: " + PlayerPrefs.GetFloat("highScore");
+            } else
+            {
+                PlayerPrefs.SetFloat("highScore", PlayerPrefs.GetFloat("playScore"));
+                HighScore.text = "Highscore: " + PlayerPrefs.GetFloat("highScore");
+            }
+        }
+
     }
     public void Restart()
     {
