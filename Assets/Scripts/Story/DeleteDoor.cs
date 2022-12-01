71ca6b6b3cab88e6da7a44c0edfa469ab86a2511
m1thraz <boehser-.-onkel@hotmail.de>
2022-12-01 21:14:46 +0200

    doors open after certain score

---
+++ Assets/Scripts/Story/DeleteDoor.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteDoor : MonoBehaviour
+{
+    [SerializeField] GameObject Door1;
+    [SerializeField] GameObject Door2;
+    [SerializeField] GameObject Door3;
+    [SerializeField] GameObject Door4;
+    [SerializeField] GameObject Door5;
+ 
+    private float score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerLogic = GameObject.FindGameObjectWithTag("Player").GetComponent(typeof(PlayerLogic)) as PlayerLogic;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        score = GetComponent<PlayerLogic>().getScore();
+        RemoveDoor();
+    }
+
+    void RemoveDoor()
+    {
+       
+        Debug.Log("score:" + score);
+        if (score == 100)
+        {
+            Destroy(Door1);
+            Debug.Log("A door opened");
+        }
+        if (score == 200)
+        {
+            Destroy(Door1);
+            Debug.Log("Door2 opened");
+        }
+        if (score == 300)
+        {
+            Destroy(Door1);
+            Debug.Log("Door3 opened");
+        }
+        if (score == 400)
+        {
+            Destroy(Door1);
+            Debug.Log("Door4 opened");
+        }
+        if (score == 500)
+        {
+            Destroy(Door1);
+            Debug.Log("Door5 opened");
+        }
+    }
+}
