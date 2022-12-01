466e27b00fad992a0d42e89e87e1b1b92ad8e8b6
m1thraz <boehser-.-onkel@hotmail.de>
2022-12-01 18:52:40 +0200

    story Rooms

--- Assets/Scripts/Story/activateBoss.cs
+++ Assets/Scripts/Story/activateBoss.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class activateBoss : MonoBehaviour
 {
-    [SerializeField] private GameObject Boss;
+    [SerializeField] private GameObject Room1;
+    [SerializeField] private GameObject Room2;
+    [SerializeField] private GameObject Room3;
+    [SerializeField] private GameObject Room4;
+    [SerializeField] private GameObject Room5;
+    private int col = 0;
+
+
+
+
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Player")
-            Boss.SetActive(true);
-        GetComponent<CircleCollider2D>().enabled = false;
-    }
+        {
+            col += 1;
+            GetComponent<CircleCollider2D>().enabled = false;
 
+        }
+    }
+    private void activateRoom(int room)
+    {
+      
+    }
 }
