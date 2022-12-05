664112f97ba7b19e6b332e534808b6395f258165
m1thraz <boehser-.-onkel@hotmail.de>
2022-12-05 19:58:29 +0200

    editet difficulty select

---
+++ Assets/1Scenes/CutScenes/Difficulty.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Difficulty : MonoBehaviour
+{
+    public void EasyMode()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void NormalMode()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void HardMode()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
